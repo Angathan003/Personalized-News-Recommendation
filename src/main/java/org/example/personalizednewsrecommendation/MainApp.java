108c259e2f44d83f0c2ca682d45b89ea6ac86d71
Angathan003 <angathan003@gmail.com>
2024-11-28 09:06:44 +0530

    admin opperations

--- src/main/java/org/example/personalizednewsrecommendation/MainApp.java
+++ src/main/java/org/example/personalizednewsrecommendation/MainApp.java
@@ -1,7 +1,6 @@
 package org.example.personalizednewsrecommendation;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.example.personalizednewsrecommendation.controllers.LoginScreen;
 import org.example.personalizednewsrecommendation.services.ArticleManager;
@@ -10,15 +9,14 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        // Path to the dataset
+        // Path to the dataset (make sure this matches your file structure)
         String datasetPath = "src/main/resources/org/example/personalizednewsrecommendation/CNN_Articels_clean.csv";
 
         // Initialize the ArticleManager with the dataset
         ArticleManager articleManager = new ArticleManager(datasetPath);
 
-        // Pass ArticleManager to the LoginScreen
+        // Set up the initial screen (LoginScreen)
         LoginScreen loginScreen = new LoginScreen(articleManager);
-
         primaryStage.setScene(loginScreen.getLoginScene(primaryStage));
         primaryStage.setTitle("Personalized News Recommendation System");
         primaryStage.show();
